7dcbdec5ffd78ca7b6be6199606f032c7e5d34c5
nakayashiki <the.masa.128@gmail.com>
2018-05-03 15:14:29 +0900

    companyモデル作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,13 @@
 class Customer < ApplicationRecord
-  validates :family_name, presence: true, length: { maximum: 20 }
+ belongs_to :company
+
+ validates :company_id, presence: true
+ validates :family_name, presence: true, length: { maximum: 20 }
  validates :given_name, presence: true, length: { maximum: 20 }
  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 
-def full_name
-  family_name + given_name
-end
+ def full_name
+   family_name + given_name
+  end
 end
